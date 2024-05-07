21ea154af0bf4bb6ba3fafb7c91a13620c13b734
Ramon Vinícius <ramon.vinicius.eng@gmail.com>
2024-05-07 19:28:53 -0300

    criação dos arquivos

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "sculptor.h"
+
+int main()
+{
+
+    return 0;
+}
---
+++ voxel.h
@@ -0,0 +1,11 @@
+#ifndef VOXEL_H
+#define VOXEL_H
+
+struct Voxel
+{
+    float r, g, b;
+    float a;
+    bool show;
+};
+
+#endif
